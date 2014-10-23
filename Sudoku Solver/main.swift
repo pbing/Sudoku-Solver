77deb660b747671bc7f1a0044afd959c0edf1f67
Bernd Beuster <bernd.beuster@googlemail.com>
2014-10-23 12:45:45 +0200

    Initial Commit

---
+++ Sudoku Solver/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Sudoku Solver
+//
+//  Created by Bernd Beuster on 23.10.14.
+//  Copyright (c) 2014 Bernd Beuster. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
