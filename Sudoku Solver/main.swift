aed46a33ba60e78c8b99b9282fd6242cfef032a0
Bernd Beuster <bernd.beuster@gmail.com>
2014-10-25 18:08:37 +0200

    Added NSFileManager() in order to have proper path for profiling with Instruments.

--- Sudoku Solver/main.swift
+++ Sudoku Solver/main.swift
@@ -10,12 +10,16 @@
 
 import Foundation
 
+/* added for profiling */
+let fileManager = NSFileManager()
+fileManager.changeCurrentDirectoryPath("/Users/bernd/Projects/Swift/Sudoku Solver/Sudoku Solver")
+
+/*
 let grid1 = "003020600900305001001806400008102900700000008006708200002609500800203009005010300"
 let grid2 = "4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......"
 let grid3 = fromFile("easy50.txt", separator: "========")[1]
 let grid4 = fromFile("top95.txt")[94]
 
-/*
 println(solve(grid1))
 println(solve(grid2))
 println(solve(grid3))
